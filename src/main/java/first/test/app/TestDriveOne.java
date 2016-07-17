6ceff6799070b6065d4ea70c1ce81efdd6866a94
pankajnimgade <pankaj.nimgade@gmail.com>
2016-07-17 18:47:23 +0530

    created gradle turorial project and tested it

---
+++ src/main/java/first/test/app/TestDriveOne.java
@@ -0,0 +1,12 @@
+package first.test.app;
+
+/**
+ * Created by Pankaj Nimgade on 17-07-2016.
+ */
+public class TestDriveOne {
+
+    public static void main(String[] args){
+
+        System.out.println("Hello World");
+    }
+}

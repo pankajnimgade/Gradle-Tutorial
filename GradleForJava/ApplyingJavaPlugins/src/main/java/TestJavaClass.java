c98a850a113dc0bac922160556819b85160e33f5
Pankaj Nimgade <pankaj.nimgade@gmail.com>
2018-05-25 23:06:54 -0500

    applying java plugins

---
+++ GradleForJava/ApplyingJavaPlugins/src/main/java/TestJavaClass.java
@@ -0,0 +1,7 @@
+public class TestJavaClass {
+
+    public static void main(String[] args) {
+        System.out.println(" this is ta test code");
+    }
+
+}
---
+++ GradleTest101/src/main/java/DriveGradleTest101.java
@@ -0,0 +1,11 @@
+/**
+ * Created on 5/25/2018
+ *
+ * @author Pankaj Nimgade
+ */
+public class DriveGradleTest101 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello Gradle");
+    }
+}

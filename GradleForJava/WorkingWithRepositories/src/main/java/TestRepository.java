5318bcebe7ca6557ec6486a403857247eba630d1
Pankaj Nimgade <pankaj.nimgade@gmail.com>
2018-05-27 14:24:43 -0500

    added external dependencies

--- GradleForJava/WorkingWithRepositories/src/main/java/TestRepository.java
+++ GradleForJava/WorkingWithRepositories/src/main/java/TestRepository.java
@@ -1,7 +1,10 @@
+import com.google.common.base.Optional;
+
 class TestRepository {
 
     public static void main(String[] args) {
         System.out.println("Test repository");
+        System.out.println("" + Optional.class);
     }
 
 }
